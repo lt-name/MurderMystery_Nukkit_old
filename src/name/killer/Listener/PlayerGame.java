56de93fc1d357cba63c208ac93dc03bed62d3d56
lt-name <572413378@qq.com>
2020-04-05 08:32:43 +0800

    添加自动兑换金锭，自动移除弓箭

--- src/name/killer/Listener/PlayerGame.java
+++ src/name/killer/Listener/PlayerGame.java
@@ -5,6 +5,10 @@ import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.entity.EntityDamageByChildEntityEvent;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
+import cn.nukkit.event.entity.EntityShootBowEvent;
+import cn.nukkit.event.player.PlayerBlockPickEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.scheduler.Task;
 import name.killer.Killer;
 import name.killer.Room.Room;
 
@@ -61,6 +65,76 @@ public class PlayerGame implements Listener {
             room.addPlaying(player2, 0);
         }
         event.setCancelled();
+    }
+
+    /**
+     * 玩家拾取物品事件
+     * @param event 事件
+     */
+    @EventHandler
+    public void onPick(PlayerBlockPickEvent event) {
+        Player player = event.getPlayer();
+        Item item = event.getItem();
+        if (player != null && item != null && item.getId() == 266) {
+            if (!Killer.getInstance().getRooms().containsKey(player.getLevel().getName())) {
+                return;
+            }
+            Killer.getInstance().getServer().getScheduler().scheduleDelayedTask(new Task() {
+                @Override
+                public void onRun(int i) {
+                    int j = 0;
+                    boolean bow = false;
+                    for (Item item : player.getInventory().getContents().values()) {
+                        if (item.getId() == 266) {
+                            j += item.getCount();
+                            continue;
+                        }
+                        if (item.getId() == 261) {
+                            bow = true;
+                        }
+                    }
+                    if (j > 9) {
+                        player.getInventory().removeItem(Item.get(266, 0, 10));
+                        player.getInventory().addItem(Item.get(262, 0, 1));
+                        if (bow) {
+                            player.getInventory().addItem(Item.get(261, 0, 1));
+                        }
+                    }
+                }
+            }, 10);
+        }
+    }
+
+    /**
+     * 生命实体射出箭 事件
+     * @param event 事件
+     */
+    @EventHandler
+    public void onShootBow(EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = ((Player) event.getEntity()).getPlayer();
+            if (!Killer.getInstance().getRooms().containsKey(player.getLevel().getName())) {
+                return;
+            }
+            if (Killer.getInstance().getRooms().get(player.getLevel().getName()).getPlayers().get(player) == 2) {
+                player.getInventory().addItem(Item.get(262, 0, 1));
+                return;
+            }
+            int j = 0; //箭的数量
+            boolean bow = false;
+            for (Item item : player.getInventory().getContents().values()) {
+                if (item.getId() == 262) {
+                    j += item.getCount();
+                    continue;
+                }
+                if (item.getId() == 261) {
+                    bow = true;
+                }
+            }
+            if (j < 1 && bow) {
+                player.getInventory().removeItem(Item.get(261, 0, 1));
+            }
+        }
     }
 
 }
