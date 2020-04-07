5334308bf68d74f20664374e5c7a445feb86b6b0
lt-name <572413378@qq.com>
2020-04-07 16:52:41 +0800

    优化

--- src/name/mysterymurder/listener/PlayerJoinAndQuit.java
+++ src/name/mysterymurder/listener/PlayerJoinAndQuit.java
@@ -9,6 +9,9 @@ import cn.nukkit.event.player.PlayerTeleportEvent;
 import cn.nukkit.scheduler.Task;
 import name.mysterymurder.MysteryMurder;
 import name.mysterymurder.room.Room;
+import name.mysterymurder.utils.Tools;
+
+import java.util.LinkedHashMap;
 
 /**
  * 玩家进入/退出服务器 或传送到其他世界时，退出房间
@@ -18,19 +21,15 @@ public class PlayerJoinAndQuit implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        MysteryMurder.getInstance().getServer().getScheduler().scheduleDelayedTask(new Task() {
-            @Override
-            public void onRun(int i) {
-                for (Room room : MysteryMurder.getInstance().getRooms().values()) {
-                    if (player != null && room.isPlaying(player)) {
-                        room.quitRoom(player, true);
-                    }
-                }
-                if (player != null && MysteryMurder.getInstance().getRooms().containsKey(player.getLevel().getName())) {
+        if (player != null && MysteryMurder.getInstance().getRooms().containsKey(player.getLevel().getName())) {
+            MysteryMurder.getInstance().getServer().getScheduler().scheduleDelayedTask(new Task() {
+                @Override
+                public void onRun(int i) {
+                    Tools.rePlayerState(player ,true);
                     player.teleport(MysteryMurder.getInstance().getServer().getDefaultLevel().getSafeSpawn());
                 }
-            }
-        }, 100);
+            }, 100);
+        }
     }
 
     @EventHandler
@@ -50,16 +49,15 @@ public class PlayerJoinAndQuit implements Listener {
         String toLevel = event.getTo().getLevel().getName();
         if (player == null || fromLevel == null || toLevel == null) { return; }
         if (!fromLevel.equals(toLevel)) {
-            if (MysteryMurder.getInstance().getRooms().containsKey(fromLevel)) {
-                Room room = MysteryMurder.getInstance().getRooms().get(fromLevel);
-                if (room.isPlaying(player)) {
-                    room.quitRoom(player, false);
-                }
-            }
-            if (!player.isOp() && MysteryMurder.getInstance().getRooms().containsKey(toLevel) &&
-                    !MysteryMurder.getInstance().getRooms().get(toLevel).isPlaying(player)) {
+            LinkedHashMap<String, Room> room =  MysteryMurder.getInstance().getRooms();
+            if (room.containsKey(fromLevel) && room.get(fromLevel).isPlaying(player)) {
                 event.setCancelled();
-                player.sendMessage("§c要进入游戏地图，请先加入游戏！");
+                player.sendMessage("§e >> §c退出房间请使用：/killer quit");
+            }
+            if (!player.isOp() && room.containsKey(toLevel) &&
+                    !room.get(toLevel).isPlaying(player)) {
+                event.setCancelled();
+                player.sendMessage("§e >> §c要进入游戏地图，请先加入游戏！");
             }
         }
     }
